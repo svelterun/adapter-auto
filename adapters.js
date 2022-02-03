e397e4ec4a384a6e7221192b6aadbda3f1ff0942
Nicholas Berlette <nick@berlette.com>
2022-02-02 23:43:01 -0800

    1.0.0-next.2

--- adapters.js
+++ adapters.js
@@ -1,44 +1,33 @@
 export const adapters = [
-	{
-		name: 'Begin',
-		test: () => !!process.env.BEGIN,
-		module: '@sveltejs/adapter-begin'
-	},
-	{
-		name: 'Vercel',
-		test: () => !!process.env.VERCEL,
-		module: '@sveltejs/adapter-vercel'
-	},
-	{
-		name: 'Cloudflare Pages',
-		test: () => !!process.env.CF_PAGES,
-		module: '@sveltejs/adapter-cloudflare'
-	},
-	{
-		name: 'Cloudflare Workers',
-		test: () => !!process.env.CF_WORKER || !!process.env.CF_WORKERS,
-		module: '@sveltejs/adapter-cloudflare-workers'
-	}
-	{
-		name: 'Netlify',
-		test: () => !!process.env.NETLIFY,
-		module: '@sveltejs/adapter-netlify'
-	},
-	{
-                name: 'Static',
-                test: () => !!process.env.STATIC,
-                module: '@sveltejs/adapter-static'
-        },
-	{
-		name: 'Node.js',
-		test: () => !!process.env.NODE || (
-			!process.env.VERCEL && 
-			!process.env.BEGIN && 
-			!process.env.CF_PAGES && 
-			!process.env.CF_WORKER && 
-			!process.env.NETLIFY &&
-			!process.env.STATIC
-		),
-		module: '@sveltejs/adapter-node'
-	}
-};
+  {
+    name: 'Cloudflare Pages',
+    test: () => !!process.env.CF_PAGES,
+    module: '@sveltejs/adapter-cloudflare'
+  },
+  {
+    name: 'Cloudflare Workers',
+    test: () => (!!process.env.CF_WORKER || !!process.env.CF_WORKERS),
+    module: '@sveltejs/adapter-cloudflare-workers'
+  },
+  {
+    name: 'Netlify',
+    test: () => !!process.env.NETLIFY,
+    module: '@sveltejs/adapter-netlify'
+  },
+  {
+    name: 'Vercel',
+    test: () => !!process.env.VERCEL,
+    module: '@sveltejs/adapter-vercel'
+  },
+  {
+    name: 'Static',
+    test: () => !!process.env.STATIC || (
+      !process.env.CF_PAGES &&
+      !process.env.CF_WORKER &&
+      !process.env.CF_WORKERS &&
+      !process.env.NETLIFY &&
+      !process.env.VERCEL
+    ),
+    module: '@sveltejs/adapter-static'
+  },
+];
